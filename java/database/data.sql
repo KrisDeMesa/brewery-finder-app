0fdfe0905ac81b53d62815e8e912714eb7e55dec
Nathan Houck <nate.houck@gmail.com>
2023-08-05 17:45:40 -0400

    began work on database schema and mock data set for breweries

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,10 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO brewery (brewery_name, hours, contact_info, address, history, active) VALUES ('Happy Times Brewing', 'All Day Every Day', '1-800-BREW-NOW', '123 Hops Lane Philadelphia PA', 'short history', true);
+INSERT INTO brewery (brewery_name, hours, contact_info, address, history, active) VALUES ('Okay Times Brewing', 'Saturday and Sunday 12pm-12am', 'info@okaytimesbrewing.com', '1 Misty Drive Philadelphia PA', 'short history', true);
+INSERT INTO brewery (brewery_name, hours, contact_info, address, history, active) VALUES ('Jumping Orca', 'M-F 6-10pm', 'jumpingorca.com', '78 Happy Way Wayne PA', 'short history', false);
+
+INSERT INTO beer (beer_name, brewery, description, abv, beer_type) VALUES ('Jumping Joseph', 1, 'It will put a hop in your step', 7.50, 'Saison');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,8 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS brewery_beer;
+DROP TABLE IF EXISTS beer;
+DROP TABLE IF EXISTS brewery;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -8,6 +11,36 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE brewery (
+	brewery_id SERIAL PRIMARY KEY,
+	brewer int,
+	brewery_name varchar(200),
+	hours varchar(200),
+	contact_info varchar(500),
+	address varchar(300),
+	history varchar(1000),
+	active boolean,
+	CONSTRAINT fk_brewery_brewer FOREIGN KEY (brewer) REFERENCES users(user_id)
+);
+
+CREATE TABLE beer (
+	beer_id SERIAL PRIMARY KEY,
+	beer_name varchar(200),
+	brewery int,
+	description varchar(600),
+	abv decimal(4,2),
+	beer_type varchar(100),
+	CONSTRAINT fk_beer_brewery FOREIGN KEY (brewery) REFERENCES brewery(brewery_id)
+);
+
+CREATE TABLE brewery_beer (
+	brewery_id int,
+	beer_id int,
+	CONSTRAINT pk_brewery_beer PRIMARY KEY (brewery_id, beer_id),
+	CONSTRAINT fk_brewery_beer_brewery_id FOREIGN KEY (brewery_id) REFERENCES brewery(brewery_id),
+	CONSTRAINT fk_brewery_beer_beer_id FOREIGN KEY (beer_id) REFERENCES beer(beer_id)
 );
 
 COMMIT TRANSACTION;
