0a35515392ac3344f860fce52fec4ba4dc1ac63c
Damian Brooks <dbrooks92200@gmail.com>
2023-08-07 14:51:31 -0400

    Code pushed

---
+++ vue/src/components/BreweryList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div> 
+      <ul>
+          <li v-for="brewery in breweryList" :key="brewery.id"> {{ brewery.name }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import breweryService from '../services/BreweryService'
+export default {
+    created() {
+        breweryService.getBreweries().then(response => {
+            this.$store.commit('GET_BREWERIES', response.data);
+    })
+    },
+    computed: {
+        breweryList() {
+            return this.$store.state.breweries;
+        }
+    },
+    methods: {
+         
+        }
+    }
+
+
+</script>
+
+<style>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,11 +2,16 @@
   <div class="home">
     <h1>Home</h1>
     <p>If you are seeing this, you are authenticated.</p>
+    <brewery-list />
   </div>
 </template>
 
 <script>
+import BreweryList from '../components/BreweryList.vue';
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    BreweryList
+  }
 };
 </script>
