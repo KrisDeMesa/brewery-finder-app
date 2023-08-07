13990fbd1295cd19e84e8aebc4b7bf25d4af3bf9
Kristoffer De Mesa <krademesa@gmail.com>
2023-08-07 19:47:56 -0400

    Main header implemented with logo and search boxes

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>If you are seeing this, you are authenticated.</p>
-    <brewery-list />
+    <!-- <app-header/> -->
+    
+    
+    <div class="filter"><h3>Filter</h3></div>
+    <div class="brewery-list"><brewery-list/></div>
+    <div class="map"><h2>Map</h2></div>
+    
   </div>
 </template>
 
 <script>
 import BreweryList from '../components/BreweryList.vue';
+// import AppHeader from '../components/AppHeader.vue'
 export default {
   name: "home",
   components: {
+    // AppHeader,
     BreweryList
   }
+    
+    
 };
 </script>
+
+<style scoped>
+* {
+  display: grid;
+    grid-template-columns: .3fr 1fr .8fr;
+    grid-template-areas: 
+    "filter brewerylist map";
+}
+h1 {
+  grid-area: map;
+}
+h3 {
+  grid-area: filter;
+}
+.brewery-list {
+  display: flex;
+  
+  grid-area: brewerylist;
+  
+}
+</style>
