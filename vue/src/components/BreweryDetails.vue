7a14c6924682be9b663bd795261e9338b0c251b3
Sun Kei Liu <skliu95@gmail.com>
2023-08-08 12:03:31 -0400

    changes

--- vue/src/components/BreweryDetails.vue
+++ vue/src/components/BreweryDetails.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="(value, key) in selectedBrewery" v-bind:key="key" > {{`${key}: ${value}`}} </li>
-        </ul>
+    <div>    
+        
+    <div id="brewery-banner">
+        <h1 v-bind:key="key"> {{selectedBrewery.name}} </h1> 
     </div>
+        <ul>
+            <li v-for="(value, key) in filteredDetails" v-bind:key="key"> 
+                 <span class='keys'>{{`${key}: `}}</span>
+                 <span>{{`${value}`}}</span>
+                 <p></p>
+            </li>
+        </ul>    
+        
+
+    </div>
+
 
 </template>
 
@@ -13,7 +24,7 @@ export default {
     data() {
         return {
             selectedBrewery: {}
-        }
+        }    
     },
     // RETRIEVE DIRECTLY FROM STORE
     // computed: {
@@ -32,10 +43,51 @@ export default {
                     this.selectedBrewery = resp.data;
                 }
             });
+    },
+    computed: {
+        filteredDetails() {
+            let filtered = {};
+            for (const key in this.selectedBrewery) {
+                if (key != 'id' && key != 'brewerId') {
+                    filtered[key] = this.selectedBrewery[key];
+                }
+            }
+            return filtered;
+        }
     }
 }
 </script>
 
 <style>
+
+#brewery-banner {
+    background-color: blue;
+    border: 1px solid black;
+    height: 220px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+h1 {
+    border: 1px solid black;
+    margin-left: 400px;
+    margin-block-start: 100px;
+    font-size: 60px;
+    color: white;
+
+}
+
+.keys {
+    text-transform: capitalize;
+    font-weight: bold;
+}
+ul {
+    list-style-type: none;
+    margin-top: 30px;
+}
+
+
+
 
 </style>
