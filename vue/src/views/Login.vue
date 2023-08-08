4dcbc7165746c109ca5cbf3c2e52f9618385b75c
Marty-Spellman <mspelljr@gmail.com>
2023-08-08 10:31:12 -0400

    erase it if you have too

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -68,4 +68,63 @@ export default {
 label {
   margin-right: 0.5rem;
 }
+body div{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: chocolate;
+}
+.centered-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 1rem;
+  flex-direction: column; 
+  /* items verticall */
+}
+
+form {
+  width: 100%; 
+  max-width: 400px; /* This will prevent the form from filling the whole page width */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-input-group {
+  margin-bottom: 1rem;
+  width: 100%;
+}
+
+label {
+  margin-bottom: 0.5rem;
+  display: block; /* Making labels block-level so they stack above the inputs */
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%; /* This ensures the inputs fill their container but not more */
+}
+
+.alert {
+  padding: 0.5rem;
+  border-radius: 4px;
+  margin-bottom: 1rem;
+  width: 100%;
+  text-align: center; /* Centering the alert text */
+}
+
+.alert-error {
+  background-color: #f8d7da;
+  color: #721c24;
+  border: 1px solid #f5c6cb;
+}
+
+.alert-success {
+  background-color: #d4edda;
+  color: #155724;
+  border: 1px solid #c3e6cb;
+}
+
 </style>
