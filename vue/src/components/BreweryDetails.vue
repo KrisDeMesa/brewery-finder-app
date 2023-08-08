f8357c856ffb2e3130ce89ef15aa6a95cf6c7878
Damian Brooks <dbrooks92200@gmail.com>
2023-08-08 15:41:28 -0400

    Brewery details page formatted

--- vue/src/components/BreweryDetails.vue
+++ vue/src/components/BreweryDetails.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>    
+    <div class="home">    
         
-    <div id="brewery-banner">
+    <div id="brewery-banner" class="banner">
         <h1 v-bind:key="key"> {{selectedBrewery.name}} </h1> 
     </div>
+        <div id="brewery-details"></div>
+        <div id="details-content"> 
+            <h2> Brewery Details</h2>
         <ul>
             <li v-for="(value, key) in filteredDetails" v-bind:key="key"> 
                  <span class='keys'>{{`${key}: `}}</span>
                  <span>{{`${value}`}}</span>
                  <p></p>
             </li>
-        </ul>    
+        </ul>
+        </div>    
         
-
+    <div id="map-label"> 
+    </div>
+    <div id="map-content">
+        <h2>Map</h2>
+    </div>
     </div>
 
 
@@ -61,20 +69,23 @@ export default {
 <style scoped>
 
 #brewery-banner {
-    background-color: blue;
+    background-image: url("../assets/brewery-banner.jpg");
+    background-position: 60% 85%;
+    filter: brightness(80%);
     border: 1px solid black;
-    height: 220px;
+    height: 300px;
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    
 }
 
 h1 {
-    border: 1px solid black;
     margin-left: 400px;
     margin-block-start: 100px;
-    font-size: 60px;
+    font-size: 80px;
     color: white;
+    
 
 }
 
@@ -87,7 +98,43 @@ ul {
     margin-top: 30px;
 }
 
+.home {
+  display: grid;
+  border: 1px solid black;
+    grid-template-columns: 2fr 2fr;
+    grid-template-areas: 
+    "banner banner"
+    "brewery-details map-label"
+    "details-content map-content";
+    row-gap: 20px;
+    
+}
+#brewery-details {
+    grid-area: brewery-details;
+}
+#map-label {
+    grid-area: map-label;
+}
+.banner {
+  grid-area: banner;
 
+}
+#details-content {
+  grid-area: details-content;
+  margin-left: 300px;
+  border: 1px solid black;
+  
+}
+#map-content {
+  grid-area: map-content;
+  margin-left: 40px;
+  margin-right: 200px;
+  border: 1px solid black;
+}
+.grid-container {
+  border: 1px solid black;
+  padding: 20px;
+}
 
 
 </style>
--- vue/src/views/BreweryDetailsView.vue
+++ vue/src/views/BreweryDetailsView.vue
@@ -1,5 +1,20 @@
 <template>
-  <brewery-details></brewery-details>
+<div class="home">
+    <!-- <app-header/> -->
+    
+    
+    <!-- <div id="details" class="grid-container">
+      <h3 id="details-header">Details</h3>
+      <div id="details-content">details content</div> -->
+      <brewery-details></brewery-details>
+      <!-- </div> -->
+
+    <!-- <div id="map" class="grid-container">
+      <h2 id="map-header">Map</h2>
+      <div id="map-content">map content</div>
+      </div> -->
+    
+  </div>
 </template>
 
 <script>
@@ -13,6 +28,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+/* .home {
+  display: grid;
+    grid-template-columns: 2fr 2fr;
+    grid-template-areas: 
+    "details-header map-header"
+    "map-content map-content";
+    row-gap: 20px;
+    
+}
+#details-header {
+  grid-area: details-header;
+}
+#map-header {
+  grid-area: map-header;
+}
+#details-content {
+  grid-area: details-content;
+}
+#map-content {
+  grid-area: map-content;
+}
+.grid-container {
+  border: 1px solid black;
+  padding: 20px;
+} */
+
+
 
 </style>
