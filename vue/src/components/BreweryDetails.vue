cbc97be0d6f7388f149a3f732027bfeade11e1af
Sun Kei Liu <skliu95@gmail.com>
2023-08-09 15:06:13 -0400

    updates

--- vue/src/components/BreweryDetails.vue
+++ vue/src/components/BreweryDetails.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="home">    
+    <div class="brewery-details">    
         
     <div id="brewery-banner" class="banner">
         <h1 v-bind:key="key"> {{selectedBrewery.name}} </h1> 
     </div>
-        <div id="brewery-details"></div>
+        <div id="details"></div>
         <div id="details-content"> 
-            <h2> Brewery Details</h2>
+            <!-- <h2> Brewery Details</h2> -->
+            <div class="head-text">Brewery Details</div>
         <ul>
             <li v-for="(value, key) in filteredDetails" v-bind:key="key"> 
                  <span class='keys'>{{`${key}: `}}</span>
@@ -15,12 +16,13 @@
             </li>
         </ul>
         </div>    
-        
-    <div id="map-label"> 
-    </div>
+     <div id="map-label">
+        <div class="head-text">Map</div>
+    </div>    
     <div id="map-content">
-        <h2>Map</h2>
+        
     </div>
+   
     </div>
 
 
@@ -71,7 +73,7 @@ export default {
 #brewery-banner {
     background-image: url("../assets/brewery-banner.jpg");
     background-position: 60% 85%;
-    filter: brightness(80%);
+    filter: brightness(90%);
     border: 1px solid black;
     height: 300px;
     display: flex;
@@ -85,8 +87,28 @@ h1 {
     margin-block-start: 100px;
     font-size: 80px;
     color: white;
+}
+.brewery-details {
+  background: rgba(251,230,194, .7);
+  display: grid;
+  border: 1px solid rgb(172, 13, 13);
+  border-radius: 10px;
+    grid-template-columns: 2fr 2fr;
+    grid-template-areas: 
+    "banner banner"
+    "details map-label"
+    "details-content map-content";
+    row-gap: 20px;
     
-
+    
+}
+.head-text {
+    background: white;
+    font-size: 40px;
+    font-weight: 900;
+    padding-left: 40px;
+    padding-top: 30px;
+    border-radius: 10px;
 }
 
 .keys {
@@ -94,47 +116,41 @@ h1 {
     font-weight: bold;
 }
 ul {
+    font-size: 20px;
     list-style-type: none;
     margin-top: 30px;
-}
-
-.home {
-  display: grid;
-  border: 1px solid black;
-    grid-template-columns: 2fr 2fr;
-    grid-template-areas: 
-    "banner banner"
-    "brewery-details map-label"
-    "details-content map-content";
-    row-gap: 20px;
+    margin-left: 30px;
     
 }
-#brewery-details {
+
+
+#details {
     grid-area: brewery-details;
 }
 #map-label {
-    grid-area: map-label;
+  color: rgb(172, 13, 13);
+  grid-area: map-content;
+  margin-left: 40px;
+  margin-right: 200px;
+  border: 1px solid rgb(172, 13, 13);
+  border-radius: 10px;
 }
 .banner {
   grid-area: banner;
 
 }
 #details-content {
+
+  color: rgb(172, 13, 13);
   grid-area: details-content;
   margin-left: 300px;
-  border: 1px solid black;
+  border: 1px solid rgb(172, 13, 13);
+  border-radius: 10px;
   
 }
 #map-content {
-  grid-area: map-content;
-  margin-left: 40px;
-  margin-right: 200px;
-  border: 1px solid black;
-}
-.grid-container {
-  border: 1px solid black;
-  padding: 20px;
-}
+  grid-area: map-label;
 
+}
 
 </style>
