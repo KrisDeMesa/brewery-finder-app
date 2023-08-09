701dcad57ff7194782472a816a8b6fecea9032a7
Marty-Spellman <mspelljr@gmail.com>
2023-08-09 14:50:09 -0400

    map component

---
+++ vue/src/components/BreweryMap.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class= "brewery-map">
+     <div id= "map">
+         <router-link >
+             <ul> {{brewerymap}} </ul>
+         </router-link>
+     </div>    
+ </div>
+</template>
+<script>
+//import // maps from "../services/MapsService";//
+// export default {};//
+// do we need to have a get for address/longtitude. I dont know how to call that//
+</script>
+<style scoped>
+
+</style>
