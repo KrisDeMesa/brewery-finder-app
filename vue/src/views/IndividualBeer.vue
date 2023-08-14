e2cdb0b70767add4d8e807338121cf6a131ac27a
Nathan Houck <nate.houck@gmail.com>
2023-08-14 16:22:50 -0400

    individual beer view

--- vue/src/views/IndividualBeer.vue
+++ vue/src/views/IndividualBeer.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
+  <div id="whole-page">
       <ind-beer-header :beer="beer"/>
       
-      <div></div>
+      <div id="main-space">
+          <div id="left-grid">
+              <p>test</p>
+          </div>
+          <div id="right-grid">
+              <p>test</p>
+          </div>
+      </div>
   </div>
 </template>
 
@@ -28,6 +35,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+#whole-page{
+    
+}
+#main-space{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: 
+    "left right";
+    border: 1px solid black;
+    
+}
+#left-grid{
+    grid-area: left;
+}
+#right-grid{
+    grid-area: right;
+}
 </style>
